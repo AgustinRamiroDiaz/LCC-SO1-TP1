c0786f20e01c68f2ac510ab6b38e2be7c7ec5caf
Agustin <agustin.ramiro.diaz@gmail.com>
2020-04-10 20:13:00 -0300

    Prueba main

--- main.c
+++ main.c
@@ -6,13 +6,15 @@ int main(int argc, char const *argv[])
 {
     board_t *tablero;
     printf("Inicializando tablero\n");
-    board_init(tablero, 3, 3);
-    printf("Cargando tablero\n");
-    board_load(tablero, "XXX\nOOO\nXXX");
-    printf("Tablero cargado\n");
-    char buff[100]; 
-    printf("Mostrando tablero\n");
-    board_show(*tablero, buff);
-    printf("%s", buff);
+    board_init_def(tablero, 3, 3, 'X');
+    printf("Imprimiendo tablero\n");
+    board_print(tablero);
+    // printf("Cargando tablero\n");
+    // board_load(tablero, "XXX\nOOO\nXXX");
+    // printf("Tablero cargado\n");
+    // char buff[100]; 
+    // printf("Mostrando tablero\n");
+    // board_show(*tablero, buff);
+    // printf("%s", buff);
     return 0;
 }
