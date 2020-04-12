aacfe73a9ce1e4a97b3ba2b0b6cf0c8d214bdc76
Ignacio <ignaciolitmanovich@gmail.com>
2020-04-12 17:18:26 -0300

    las funciones ahora toman direccion de memoria

--- Board.h
+++ Board.h
@@ -33,10 +33,10 @@ int board_init(board_t *board, size_t row, size_t col);
 int board_init_def(board_t *board, size_t row, size_t col, char def);
 
 /* Leer el tablero en una posición (col, row) */
-char board_get(board_t board, size_t row, size_t col);
+char board_get(board_t *board, size_t row, size_t col);
 
 /* Asignarle un valor 'val' a la posición (col, row) del tablero*/
-int board_set(board_t board, size_t row, size_t col, char val);
+int board_set(board_t *board, size_t row, size_t col, char val);
 
 /* Leer de una lista de caracteres e interpretarla como un tablero */
 int board_load(board_t *board, char *str);
@@ -45,10 +45,10 @@ void board_load_from_file(board_t *board, const char *nombreArchivo);
 
 /* Función para mostrar el tablero */
 /* La función 'board_show' asume que hay espacio suficiente en 'res' para alojar el tablero.*/
-void board_show(board_t board, char *res);
+void board_show(board_t *board, char *res);
 
 /* Función para mostrar el tablero por panatalla*/
-void board_print(board_t board);
+void board_print(board_t *board);
 
 /* Destroy board */
 void board_destroy(board_t *board);
